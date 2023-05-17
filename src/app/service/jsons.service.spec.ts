d4d76d2b43c68f2b40f8fa06257ed63c39d8e163
KRUTIKHIRAPARA <krutik2511@gmail.com>
2023-05-17 11:57:51 +0530

    Get Data Test Case

--- src/app/service/jsons.service.spec.ts
+++ src/app/service/jsons.service.spec.ts
@@ -2,11 +2,16 @@ import { TestBed } from '@angular/core/testing';
 
 import { JsonsService } from './jsons.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('JsonsService', () => {
-  let service: JsonsService;
+  let jsonService: JsonsService;
+  let mockHttpClient;
 
   beforeEach(() => {
+
+    jsonService =  new JsonsService(mockHttpClient);
+
     TestBed.configureTestingModule({
       imports:[
         HttpClientModule
@@ -15,10 +20,39 @@ describe('JsonsService', () => {
         HttpClient,
       ]
     });
-    service = TestBed.inject(JsonsService);
+    jsonService = TestBed.inject(JsonsService);
+
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(jsonService).toBeTruthy();
   });
+
+  // Check GET method are return respose is valid or not
+  it('should be return list datas', () => {
+    let mockResponse = [
+      {
+        taskItems: [
+          {
+            isCompleted: false,
+            item: "Create CRUD"
+          },
+          {
+            isCompleted: true,
+            item: "UI To-do List App"
+          }
+        ],
+        date: "2023-05-16T04:45:50.470Z",
+        taskName: "Work",
+        id: 2
+      }
+    ];
+
+    let respose;
+    spyOn(jsonService,'getListData').and.returnValue(of(mockResponse));
+    jsonService.getListData().subscribe(res => {respose = res})
+
+    expect(respose).toEqual(mockResponse);
+  });
+
 });
