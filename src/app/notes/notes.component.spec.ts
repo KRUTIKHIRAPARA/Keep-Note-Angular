0889b1f55dc1971e3bf8a948dc3a06db304e6b4c
KRUTIKHIRAPARA <krutik2511@gmail.com>
2023-05-17 17:34:42 +0530

    Test Case Changes

--- src/app/notes/notes.component.spec.ts
+++ src/app/notes/notes.component.spec.ts
@@ -4,12 +4,38 @@ import { NotesComponent } from './notes.component';
 import { ToastrModule } from 'ngx-toastr';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('NotesComponent', () => {
   let component: NotesComponent;
   let fixture: ComponentFixture<NotesComponent>;
+  let LISTS;
+  let mockToastrService;
+  let mockJsonService;
 
-  beforeEach(async () => {
+  beforeEach(async () => { 
+
+    LISTS = [
+      {
+        taskItems: [
+          {
+            isCompleted: false,
+            item: "Create CRUD"
+          },
+          {
+            isCompleted: true,
+            item: "UI To-do List App"
+          }
+        ],
+        date: "2023-05-16T04:45:50.470Z",
+        taskName: "Work",
+        id: 2
+      }
+    ];
+    
+    mockJsonService = jasmine.createSpyObj(['getAllList','addNewList','editList','deleteList']);
+    component =  new NotesComponent(mockToastrService,mockJsonService);
+
     await TestBed.configureTestingModule({
       declarations: [ NotesComponent ],
       imports:[
@@ -30,5 +56,29 @@ describe('NotesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Get Lists
+  it('should get the all lists data',()=>{
+    mockJsonService.getAllList.and.returnValue(of(LISTS));
+    component.getlistArray = LISTS;
+    component.getAllList();
+    expect(component.getlistArray).toBe(LISTS);
+  });
+
+  // Add List
+  it('should add the list',()=>{
+    mockJsonService.addNewList.and.returnValue(of(true));
+    component.getlistArray = LISTS;
+    component.addNewList();
+    expect(component.getlistArray).toBe(LISTS);
+  });
+
+  // Delete List
+  it('should remove the list',()=>{
+    mockJsonService.deleteList.and.returnValue(of(true));
+    component.getlistArray = LISTS;
+    component.deleteList(LISTS);
+    expect(component.getlistArray.id).toBe(LISTS.id);
   });
 });
